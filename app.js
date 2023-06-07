9c1109b629ffe5b75b1b16eba5480f0ab26efd53
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-07 19:00:38 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const wrapper = document.querySelector(".wrapper");
+const ticket = document.querySelector(".ticket");
+const { width, height } = wrapper.getBoundingClientRect();
+const halfWidth = width / 2;
+const halfHeight = height / 2;
+
+window.addEventListener("mousemove", (event) => {
+  ticket.style.transition = "none";
+  const { pageX, pageY } = event;
+
+  const rotationX = ((pageX - halfWidth) / halfWidth) * 10;
+  const rotationY = ((pageY - halfHeight) / halfHeight) * 10;
+
+  ticket.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg) `;
+});
+window.addEventListener("mouseleave", () => {
+  ticket.style.transition = "transform .5s ease-in-out";
+  ticket.style.transform = `rotateX(0deg) rotateY(0deg) `;
+});
