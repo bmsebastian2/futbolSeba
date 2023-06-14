e8b61058195864230f615788af3368622c8319dc
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-14 14:01:20 -0300

    style bien app para mobil

--- app.js
+++ app.js
@@ -4,26 +4,32 @@ const { width, height } = wrapper.getBoundingClientRect();
 const halfWidth = width / 2;
 const halfHeight = height / 2;
 
-window.addEventListener("deviceorientation", handleOrientation, true);
-function handleOrientation(event) {
-  const absolute = event.absolute;
-  const alpha = event.alpha;
-  const beta = event.beta;
-  const gamma = event.gamma;
+if (window.innerWidth <= 768) {
+  function handleOrientation(event) {
+    ticket.style.transition = "none";
+    const absolute = event.absolute;
+    // const alpha = event.alpha;
+    const x = event.beta;
+    const y = event.gamma;
 
-  console.log(absolute);
+    const roX = ((x - halfWidth) / halfWidth) * 10;
+    const roY = ((y - halfHeight) / halfHeight) * 10;
+
+    ticket.style.transform = `rotateX(${roY}deg) rotateY(${roX}deg) `;
+  }
+  window.addEventListener("deviceorientation", handleOrientation, true);
+} else {
+  window.addEventListener("mousemove", (event) => {
+    ticket.style.transition = "none";
+    const { pageX, pageY } = event;
+
+    const rotationX = ((pageX - halfWidth) / halfWidth) * 10;
+    const rotationY = ((pageY - halfHeight) / halfHeight) * 10;
+
+    ticket.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg) `;
+  });
+  window.addEventListener("mouseleave", () => {
+    ticket.style.transition = "transform .5s ease-in-out";
+    ticket.style.transform = `rotateX(0deg) rotateY(0deg) `;
+  });
 }
-
-window.addEventListener("mousemove", (event) => {
-  ticket.style.transition = "none";
-  const { pageX, pageY } = event;
-
-  const rotationX = ((pageX - halfWidth) / halfWidth) * 10;
-  const rotationY = ((pageY - halfHeight) / halfHeight) * 10;
-
-  ticket.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg) `;
-});
-window.addEventListener("mouseleave", () => {
-  ticket.style.transition = "transform .5s ease-in-out";
-  ticket.style.transform = `rotateX(0deg) rotateY(0deg) `;
-});
