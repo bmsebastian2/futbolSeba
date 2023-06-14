97fcaebab7988ae784d167254dfa53634669d9c8
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-14 14:32:27 -0300

    PhoneMobil 2

--- app.js
+++ app.js
@@ -11,13 +11,18 @@ if (window.innerWidth <= 768) {
     ticket.style.transition = "none";
     // const absolute = event.absolute;
     // const alpha = event.alpha;
-    const x = event.beta;
-    const y = event.gamma;
+    const y = event.beta;
+    const x = event.gamma;
+    var windowWidth = window.innerWidth;
+    var maxX = windowWidth - cuadrado.offsetWidth;
+    var maxY = windowHeight - cuadrado.offsetHeight;
+    var posX = (x / 90) * maxX; // Movimiento en el eje X
+    var posY = (y / 90) * maxY; // Movimiento en el eje Y
 
     const roX = ((x - halfWidth) / halfWidth) * 10;
     const roY = ((y - halfHeight) / halfHeight) * 10;
 
-    ticket.style.transform = `rotateX(${roX}deg) rotateY(${roY}deg) `;
+    ticket.style.transform = `rotateX(${posX}deg) rotateY(${posY}deg) `;
   }
 } else {
   window.addEventListener("mousemove", (event) => {
