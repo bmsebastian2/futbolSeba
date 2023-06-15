6a04f514f46908d5e2f29b7138866a7506215373
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-06-15 19:54:10 -0300

    Primera prueba movil device mobil

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ const halfWidth = width / 2;
 const halfHeight = height / 2;
 
 if (window.innerWidth <= 968) {
-  alert("device");
+  if (window.DeviceMotionEvent) {
+    window.addEventListener("devicemotion", handleMotion);
+
+    function handleMotion(event) {
+      const ax = Math.round(event.accelerationIncludingGravity.x);
+      const ay = Math.round(event.accelerationIncludingGravity.y);
+      ticket.style.transition = "none";
+
+      ticket.style.transform = `rotateX(${ax * 4}deg) rotateY(${ay * 4}deg)`;
+      if (ax === 0 && ay === 0) {
+        ticket.style.transition = "transform .5s ease-in-out";
+        ticket.style.transform = `rotateX(0deg) rotateY(0deg)`;
+      }
+    }
+  }
 } else {
   window.addEventListener("mousemove", (event) => {
     ticket.style.transition = "none";
